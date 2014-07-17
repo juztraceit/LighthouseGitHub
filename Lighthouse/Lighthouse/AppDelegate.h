97e86c803b2d39080c026ef8138cda98f99788f8
juztraceit <timothyfigura@Timothys-MacBook-Air.local>
2014-07-17 16:02:52 -0400

    Initial Commit

---
+++ Lighthouse/Lighthouse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lighthouse
+//
+//  Created by Timothy Figura on 7/17/14.
+//  Copyright (c) 2014 Timothy Figura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
